599c390822c8c82301bdb2fb85bc36c37b17e10b
onoerror <a3575131c@gmail.com>
2016-04-07 09:59:39 +0800

    動物的誘惑

--- app/src/main/java/com/quizmagic/flashgame/MainActivity.java
+++ app/src/main/java/com/quizmagic/flashgame/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 
@@ -22,9 +23,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView m_logo_name;
     private TextView m_view_message;
     private Button m_btn_go;
+    private SeekBar m_skb_duration;
+    private TextView m_tv_duration;
 
-    private TypedArray mNbaLogos;
-    private int mNbaLogosCount;
+    private TypedArray mNbaLogos;//資源檔陣列
+    private int mNbaLogosCount;//一共有多少張圖
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         initView();
         initFrameAnimation();
         initNbaLogos();
+        initSeekBar();
     }
 
     private void initNbaLogos(){
@@ -43,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private TypedArray getNbaLogos(){
-
+        //取得 nba logo drawables
         TypedArray logos = getResources().obtainTypedArray(R.array.nba_logos);
         return logos;
     }
@@ -56,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
         m_logo_name = (TextView)findViewById(R.id.tv_logo_name);
         m_tv_message = (TextView)findViewById(R.id.view_message);
         m_btn_go = (Button)findViewById(R.id.m_btn_go);
+
+        m_skb_duration = (SeekBar)findViewById(R.id.skb_duration);
+        m_tv_duration = (TextView)findViewById(R.id.tv_duration);
     }
 
     private void initFrameAnimation(){
@@ -76,56 +83,92 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
-
+    //Handler 處理代辦事項
     private Handler m_Handler = new Handler();
 
     private void animation5secs(){
-        int delayMillis = 5 * 1000;
+        int delayMillis = 5 * 1000;//5秒
+        //建立工作
         Runnable task = new Task();
-
+        //交付工作，並在指定時間過後執行
         boolean result = m_Handler.postDelayed(task,delayMillis);
 
         m_tv_message.setText(result ?"交付成功":"交付失敗");
-        m_fram_animation.start();
+        m_fram_animation.start();//開始動畫
 
     }
-
+    //按下go
     public void go(View view) {
-        m_Handler.post(mStart);
-        m_Handler.postDelayed(mStop, 3000);
-        m_btn_go.setEnabled(false);
+        m_Handler.post(mStart);//立刻執行任務 隨機換圖
+        m_Handler.postDelayed(mStop, 20_000);//3秒後執行任務 停止隨機換圖
+        m_btn_go.setEnabled(false);//go按鈕不可以案
     }
-
+    //將來被執行的工作(內部類別，存在於外部類別實體當中，像 引擎 存在於 車子 實體)
     private class Task implements Runnable{
         @Override
-        public void run(){
-
-            m_fram_animation.stop();
+        public void run(){//工作內容
+            //可直接存取 外部類別實體的成員(包含 Private 成員)
+            m_fram_animation.stop();//結束動畫
             m_tv_message.setText("時間到");
         }
     }
-
+    //建立任務物件
     private Start mStart = new Start();
     private Stop mStop = new Stop();
-
+    //任務 開始 隨機換圖
     private class Start implements Runnable{
         @Override
         public void run(){
-
+            //隨機產生 0 ~ 圖數量-1
             int index = (int)(Math.random()*mNbaLogosCount);
-
+            //換圖
             m_view_logo.setBackground(mNbaLogos.getDrawable(index));
-
-            m_Handler.postDelayed(this, 10);
+            //0.1秒後再執行一次本任務
+            m_Handler.postDelayed(this, mDuration);
         }
     }
+    //任務 停止 隨機換圖
     private class Stop implements Runnable{
         @Override
         public void run(){
-
+            //取消任務 StartRandomTask
             m_Handler.removeCallbacks(mStart);
-
+            //go按鈕恢復能按
             m_btn_go.setEnabled(true);
         }
     }
+
+    private int mDuration;//隨機間隔換圖時間
+
+    private void initSeekBar(){
+        m_tv_duration.setText(String.valueOf((mDuration)));//顯示目前所設定的間隔時間
+        m_skb_duration.setMax(20);//SeekBar 可拖曳的最大值，最小值固定為0(不可改)
+
+        //設定 當 SeekBar被操作時要執行什麼
+        m_skb_duration.setOnSeekBarChangeListener(new 屠龍刀());
+    }
+
+    private class 屠龍刀 implements SeekBar.OnSeekBarChangeListener{
+
+        //拖曳 SeekBar
+        @Override
+        public void onProgressChanged(SeekBar seekBar,int progress,boolean fromUser){
+            //progress 代表目前拖曳中所代表的值
+            int duration = progress*50;
+            m_tv_duration.setText(String.valueOf(duration));//更新目前顯示
+            mDuration = duration;
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    }
+
+
 }
